4b288259c631847e35443ca238c997b12915a229
Wernerrrr <157411888+Wernerrrr@users.noreply.github.com>
2024-09-06 23:16:15 +0100

    Final

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery')
+
+const createGalleryMarkup = (items) => {
+    return items
+        .map((item) => {
+            const{preview, original, description} = item;
+            return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}" >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+        })
+        .join('')
+}
+
+const renderGallery = () => {
+    galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+};
+
+renderGallery();
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    captionsData: 'alt',
+});
